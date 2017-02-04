d81da21b1f80d301563dfe2e4d16a12325423a9e
KevinKarlberg <Kevinkarlberg@hotmail.se>
2017-02-04 11:43:02 +0100

    Updated the solution with the two additional projects it will include

---
+++ MyGame.Database/MyGame.Database/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyGame.Database.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        T GetById(Guid id);
+        void Add(T entity);
+        void Update(T entity);
+        bool Delete(T entity);
+        bool Delete(Guid id);
+
+    }
+}
---
+++ MyGame.Database/MyGame.MVCSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyGame.MVCSite.Startup))]
+namespace MyGame.MVCSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
