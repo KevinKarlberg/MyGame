b2ce2a8c157340f0b581010c30f9223838d61893
KevinKarlberg <Kevinkarlberg@hotmail.se>
2017-02-04 18:07:28 +0100

    Completed LocationRepository

--- MyGame.Database/MyGame.Database/Repositories/LocationRepository.cs
+++ MyGame.Database/MyGame.Database/Repositories/LocationRepository.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace MyGame.Database.Repositories
 {
@@ -16,27 +17,80 @@ namespace MyGame.Database.Repositories
         }
         public IQueryable<Location> GetAll()
         {
-            throw new NotImplementedException();
+            var locations = new List<Location>();
+
+            using (ctx)
+            {
+                locations = ctx.Locations
+                     .Include(l => l.GalaxyNumber)
+                     .Include(l => l.LocalCluster)
+                     .Include(l => l.SystemNumber)
+                     .ToList();
+            }
+            return locations.AsQueryable();
         }
         public Location GetById(Guid id)
         {
-            throw new NotImplementedException();
+            Location location;
+            using (ctx)
+            {
+                location = ctx.Locations.Where(b => b.LocationId == id)
+                     .Include(l => l.GalaxyNumber)
+                     .Include(l => l.LocalCluster)
+                     .Include(l => l.SystemNumber)
+                    .FirstOrDefault();
+            }
+            return location;
         }
         public bool Delete(Location location)
         {
-            throw new NotImplementedException();
+            return Delete(location.LocationId);
         }
         public bool Delete(Guid id)
         {
-            throw new NotImplementedException();
+            using (ctx)
+            {
+                var location = ctx.Locations.Where(b => b.LocationId == id)
+                       .Include(l => l.GalaxyNumber)
+                       .Include(l => l.LocalCluster)
+                       .Include(l => l.SystemNumber)
+                       .FirstOrDefault();
+                if (location != null)
+                {
+                    ctx.Locations.Remove(location);
+                    ctx.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
         }
         public void Update(Location location)
         {
-            throw new NotImplementedException();
+            using (ctx)
+            {
+                var item = ctx.Locations.Where(b => b.LocationId == location.LocationId).FirstOrDefault();
+                if (item != null)
+                {
+                    item.GalaxyNumber = location.GalaxyNumber;
+                    item.LocalCluster = location.LocalCluster;
+                    item.SystemNumber = location.SystemNumber;
+                }
+            }
         }
         public void Add(Location location)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (ctx)
+                {
+                    ctx.Locations.Add(location);
+                    ctx.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
