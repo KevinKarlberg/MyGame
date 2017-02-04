2dec691849a796a8ea02f690a8fd8d927b4dc883
KevinKarlberg <Kevinkarlberg@hotmail.se>
2017-02-04 12:06:42 +0100

    Added BuildingsRepository (not implemented yet)

---
+++ MyGame.Database/MyGame.Database/Repositories/BuildingsRepository.cs
@@ -0,0 +1,42 @@
+﻿using MyGame.Database.DB.Models;
+using MyGame.Database.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyGame.Database.Repositories
+{
+    public class BuildingsRepository : IRepository<Buildings>
+    {
+        private MyGameDBContext ctx;
+        public BuildingsRepository()
+        {
+            this.ctx = new MyGameDBContext();
+        }
+        public IQueryable<Buildings> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public Buildings GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Delete(Buildings building)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Delete(Guid id) 
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(Buildings building)
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(Buildings building)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
